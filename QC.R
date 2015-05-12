b9ca23093b24922ee45a53af4efa534b82553804
Kentsis Research Group <kentsisresearchgroup@gmail.com>
2015-05-12 16:18:57 -0400

    Update on QC

--- QC.R
+++ QC.R
@@ -1,29 +1,38 @@
-# data <- read.table("tp_data.csv", sep=',', header = T)
-
-# This function performs basic QC for the analysis
+# This function performs basic QC befor analysis
 
 QC <- function(data){
   data$X <- NULL # reading in df gives an extra column. How to avoid?
+  # Check for the correct number of columns. The input must be in the required order
   if(ncol(data)!=9){
     stop(message("Input does not contain the required number of columns. \n"))
   }
+  # Set column names to what is required for the remainder of the analysis
   colnames(data) <- c("Protein", "Peptide", "Modification", "Position", "Condition",
                       "PatientID", "Label", "Run", "Intensity")
-  if(sum(data$Intensity<=3000)!=0) {
-    sprintf("Peptides %s for patients %s runs %s has 0 intensity: Peptide is removed from further analysis",
-            data$Peptide[data$Intensity<=3000],data$PatientID[data$Intensity<=3000],data$Run[data$Intensity<=3000])
-  }
-  data <- data[-which(data$Intensity<=3000),]
-  if(sum(is.na(data$Intensity))!=0) {
-    sprintf("Peptides %s for patients %s runs %s has 0 intensity: Peptide is removed from further analysis",
-            data$Peptide[data$Intensity<=3000],data$PatientID[data$Intensity<=3000],data$Run[data$Intensity<=3000])
-    data <- data[-which(is.na(data$Intensity)),]
+  # Report intensity values that are zero or missing (NA) and remove from further analysis
+  if(sum(data$Intensity<=0)!=0) {
+    cat(sprintf("%s-labeled peptide %s for patient %s condition %s run %s has 0 intensity: Peptide is removed from further analysis \n",
+            data$Label[data$Intensity<=0], data$Peptide[data$Intensity<=0],data$PatientID[data$Intensity<=0],
+            data$Condition[data$Intensity<=0],data$Run[data$Intensity<=0]))
+    data <- data[-which(data$Intensity<=0),]
   }
   
+  if(sum(is.na(data$Intensity))!=0) {
+    cat(sprintf("%s-labeled peptide %s for patient %s condition %s run %s has NA as intensity: Peptide is removed from further analysis \n",
+            data$Label[is.na(data$Intensity)], data$Peptide[is.na(data$Intensity)],data$PatientID[is.na(data$Intensity)],
+            data$Condition[is.na(data$Intensity)],data$Run[is.na(data$Intensity)])) 
+    data <- data[-which(is.na(data$Intensity)),]  
+  }
+
+  
+  
+  #Turn contents into factors 
   data$Protein <- factor(data$Protein)
   data$Peptide <- factor(data$Peptide)
-  data$Modi <- factor(data$Peptide)
-  
+  data$Modification <- factor(data$Modification)
+  data$Condition <- factor(data$Condition)
+  data$PatientID <- factor(data$PatientID)
+  data$Label <- factor(data$Label)
   
   return(data)
 }
